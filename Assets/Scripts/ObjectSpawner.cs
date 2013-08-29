7fc222a8cbb08b8214bf14dd82eb5d42b817e62d
Dan Erickson <dan.erickson@lostcreekconsulting.com>
2013-08-29 13:30:32 -0700

    add an object spawning script, and a basic scene to test it

---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObjectSpawner : MonoBehaviour {
+	
+	public List<GameObject> SpawnableObjects;
+	private int _selectedIndex = 0;
+	public float Distance;
+	
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire2")) {
+			Debug.Log ("Switch Objects");
+			_selectedIndex = (_selectedIndex+1) % SpawnableObjects.Count;
+		}
+		
+		if (Input.GetButtonDown ("Fire1")) {
+			Debug.Log ("Spawn Object");
+			if (_selectedIndex >= SpawnableObjects.Count)
+				_selectedIndex = SpawnableObjects.Count-1;
+			if (_selectedIndex >= 0) {
+				Instantiate (SpawnableObjects[_selectedIndex], transform.position, Quaternion.identity);
+			}
+		}
+	}
+}
