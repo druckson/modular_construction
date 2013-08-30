6e718f2afa1df43734d838e189ac11c5acb6bc42
Dan Erickson <dan.erickson@lostcreekconsulting.com>
2013-08-29 18:19:48 -0700

    add construction manager script

---
+++ Assets/Scripts/ConstructionManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConstructionManager : MonoBehaviour {
+	public float Distance = 10f;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var hit = new RaycastHit();
+		Physics.Raycast (transform.position, transform.forward, out hit, Distance);
+		if (hit.transform.gameObject)
+			Object.Destroy (hit.transform.gameObject);
+	}
+}
